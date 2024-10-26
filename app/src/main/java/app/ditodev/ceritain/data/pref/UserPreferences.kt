511aa76923e70e0d677927986a1c85f62559c16e
Gorthdzx <krislaksana123@gmail.com>
2024-10-26 18:30:52 +0700

    feat : added user preferences

--- app/src/main/java/app/ditodev/ceritain/data/pref/UserPreferences.kt
+++ app/src/main/java/app/ditodev/ceritain/data/pref/UserPreferences.kt
@@ -3,7 +3,6 @@ package app.ditodev.ceritain.data.pref
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
@@ -16,18 +15,18 @@ val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "se
 class UserPreferences private constructor(private val dataStore: DataStore<Preferences>) {
     suspend fun saveSession(user: UserModel) {
         dataStore.edit {
-            it[EMAIL_KEY] = user.email
+            it[USER_ID] = user.userId
+            it[NAME_KEY] = user.name
             it[TOKEN_KEY] = user.token
-            it[IS_LOGIN_KEY] = true
         }
     }
 
     fun getSession(): Flow<UserModel> {
         return dataStore.data.map {
             UserModel(
-                it[EMAIL_KEY] ?: "",
-                it[TOKEN_KEY] ?: "",
-                it[IS_LOGIN_KEY] ?: false
+                it[USER_ID] ?: "",
+                it[NAME_KEY] ?: "",
+                it[TOKEN_KEY] ?: ""
             )
         }
     }
@@ -39,9 +38,9 @@ class UserPreferences private constructor(private val dataStore: DataStore<Prefe
     }
 
     companion object {
-        private val EMAIL_KEY = stringPreferencesKey("email")
+        private val USER_ID = stringPreferencesKey("userId")
+        private val NAME_KEY = stringPreferencesKey("name")
         private val TOKEN_KEY = stringPreferencesKey("token")
-        private val IS_LOGIN_KEY = booleanPreferencesKey("isLogin")
 
         @Volatile
         private var instance: UserPreferences? = null
