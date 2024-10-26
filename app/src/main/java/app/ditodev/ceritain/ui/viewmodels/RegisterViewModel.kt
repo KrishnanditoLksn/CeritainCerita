0af40391ff91dd8554c23798b7b92aa6ba31453c
Gorthdzx <krislaksana123@gmail.com>
2024-10-26 11:53:55 +0700

    remove : remove viewmodel lanch

--- app/src/main/java/app/ditodev/ceritain/ui/viewmodels/RegisterViewModel.kt
+++ app/src/main/java/app/ditodev/ceritain/ui/viewmodels/RegisterViewModel.kt
@@ -1,9 +1,7 @@
 package app.ditodev.ceritain.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import app.ditodev.ceritain.data.repository.RegisterRepository
-import kotlinx.coroutines.launch
 
 class RegisterViewModel(private val repository: RegisterRepository) :
     ViewModel() {
@@ -11,13 +9,5 @@ class RegisterViewModel(private val repository: RegisterRepository) :
         name: String,
         email: String,
         password: String
-    ) {
-        viewModelScope.launch {
-            repository.handleRegistration(
-                name = name,
-                email = email,
-                password = password
-            )
-        }
-    }
+    ) = repository.handleRegistration(name, email, password)
 }
