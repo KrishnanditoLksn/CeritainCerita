01b40cd2b5c03606814ee15e7a7fa922f6cad06a
Gorthdzx <krislaksana123@gmail.com>
2024-10-26 22:51:23 +0700

    feat: added dependency injection for story repository

--- app/src/main/java/app/ditodev/ceritain/data/di/DependencyInjection.kt
+++ app/src/main/java/app/ditodev/ceritain/data/di/DependencyInjection.kt
@@ -1,11 +1,18 @@
 package app.ditodev.ceritain.data.di
 
+import android.content.Context
+import app.ditodev.ceritain.data.pref.UserPreferences
+import app.ditodev.ceritain.data.pref.dataStore
 import app.ditodev.ceritain.data.remote.api.ApiConfig
-import app.ditodev.ceritain.data.repository.RegisterRepository
+import app.ditodev.ceritain.data.repository.StoryRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 object DependencyInjection {
-    fun provideRegisterRepository(): RegisterRepository {
-        val apiService = ApiConfig.getApiService()
-        return RegisterRepository.getInstance(apiService)
+    fun provideRepository(context: Context): StoryRepository {
+        val pref = UserPreferences.getInstance(context.dataStore)
+        val user = runBlocking { pref.getSession().first() }
+        val apiService = ApiConfig.getApiService(user.token)
+        return StoryRepository.getInstance(apiService, pref)
     }
 }
