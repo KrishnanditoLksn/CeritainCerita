0cb39d8bf8ed4401872418c7380a8273acd14149
Gorthdzx <krislaksana123@gmail.com>
2024-10-26 11:52:54 +0700

    remove :  viewmodel scope launch register repository

--- app/src/main/java/app/ditodev/ceritain/data/repository/RegisterRepository.kt
+++ app/src/main/java/app/ditodev/ceritain/data/repository/RegisterRepository.kt
@@ -1,20 +1,32 @@
 package app.ditodev.ceritain.data.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
 import app.ditodev.ceritain.data.remote.api.ApiService
+import app.ditodev.ceritain.data.remote.response.ErrorResponse
+import app.ditodev.ceritain.data.remote.response.RegisterResponse
+import app.ditodev.ceritain.data.result.Result
+import com.google.gson.Gson
+import retrofit2.HttpException
 
 class RegisterRepository(
     private val apiService: ApiService
 ) {
-    suspend fun handleRegistration(
+    fun handleRegistration(
         name: String,
         email: String,
         password: String
-    ) {
-        apiService.register(
-            name = name,
-            email = email,
-            password = password
-        )
+    ): LiveData<Result<RegisterResponse>> = liveData {
+        emit(Result.Loading)
+        try {
+            val response = apiService.register(name = name, email = email, password = password)
+            emit(Result.Success(response))
+        } catch (e: HttpException) {
+            val jsonInString = e.response()?.errorBody()?.string()
+            val errorBody = Gson().fromJson(jsonInString, ErrorResponse::class.java)
+            val errorMessage = errorBody.message
+            emit(Result.Error(errorMessage.toString()))
+        }
     }
 
     companion object {
