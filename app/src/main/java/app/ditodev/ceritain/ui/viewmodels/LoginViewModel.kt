2a98ec40f1e237e9dad84ed9a97381f391f68a19
Gorthdzx <krislaksana123@gmail.com>
2024-10-27 10:15:49 +0700

    REFACTOR : repository name parameter

--- app/src/main/java/app/ditodev/ceritain/ui/viewmodels/LoginViewModel.kt
+++ app/src/main/java/app/ditodev/ceritain/ui/viewmodels/LoginViewModel.kt
@@ -1,10 +1,10 @@
 package app.ditodev.ceritain.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
-import app.ditodev.ceritain.data.repository.LoginRepository
+import app.ditodev.ceritain.data.repository.StoryRepository
 
 class LoginViewModel(
-    private val repository: LoginRepository
+    private val repository: StoryRepository
 ) : ViewModel() {
     fun handleLogin(email: String, password: String) = repository.handleLogin(email, password)
 }
