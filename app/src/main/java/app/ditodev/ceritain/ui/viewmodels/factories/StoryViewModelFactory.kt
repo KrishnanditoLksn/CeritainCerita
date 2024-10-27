92b87d247f27e2b3ba28716919b4399bf4647141
Gorthdzx <krislaksana123@gmail.com>
2024-10-27 20:54:28 +0700

    FEAT: display viewmodel factory class

--- app/src/main/java/app/ditodev/ceritain/ui/viewmodels/factories/StoryViewModelFactory.kt
+++ app/src/main/java/app/ditodev/ceritain/ui/viewmodels/factories/StoryViewModelFactory.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import app.ditodev.ceritain.data.di.DependencyInjection
 import app.ditodev.ceritain.data.repository.StoryRepository
+import app.ditodev.ceritain.ui.viewmodels.DisplayStoryViewModel
 import app.ditodev.ceritain.ui.viewmodels.LoginViewModel
 import app.ditodev.ceritain.ui.viewmodels.MainViewModel
 import app.ditodev.ceritain.ui.viewmodels.RegisterViewModel
@@ -24,6 +25,10 @@ class StoryViewModelFactory(private val repository: StoryRepository) :
 
             modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
                 RegisterViewModel(repository) as T
+            }
+
+            modelClass.isAssignableFrom(DisplayStoryViewModel::class.java) -> {
+                DisplayStoryViewModel(repository) as T
             }
 
             else -> {
