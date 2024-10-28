e7db657bad970a827a9afee2a25a85b7a9c67057
Gorthdzx <krislaksana123@gmail.com>
2024-10-29 05:59:06 +0700

    feat : take image from gallery

--- app/src/main/java/app/ditodev/ceritain/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/app/ditodev/ceritain/ui/dashboard/DashboardFragment.kt
@@ -1,42 +1,61 @@
 package app.ditodev.ceritain.ui.dashboard
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import app.ditodev.ceritain.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var currentImage: Uri? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnGallery.setOnClickListener {
+            startGallery()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri ->
+        if (uri != null) {
+            currentImage = uri
+            showImage()
+        } else {
+            Toast.makeText(activity, "No Media Selected", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun showImage() {
+        currentImage?.let {
+            binding.topImage.setImageURI(it)
+        }
     }
 }
