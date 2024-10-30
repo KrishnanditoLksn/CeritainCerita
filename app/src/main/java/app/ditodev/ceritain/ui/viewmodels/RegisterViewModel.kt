2d43358f3389efc60107dcf975a8d494996aa67b
Gorthdzx <krislaksana123@gmail.com>
2024-10-30 07:03:46 +0700

    test : added register viewmodel unit test

--- app/src/main/java/app/ditodev/ceritain/ui/viewmodels/RegisterViewModel.kt
+++ app/src/main/java/app/ditodev/ceritain/ui/viewmodels/RegisterViewModel.kt
@@ -1,5 +1,6 @@
 package app.ditodev.ceritain.ui.viewmodels
 
+import androidx.core.util.PatternsCompat
 import androidx.lifecycle.ViewModel
 import app.ditodev.ceritain.data.repository.StoryRepository
 
@@ -10,4 +11,20 @@ class RegisterViewModel(private val repository: StoryRepository) :
         email: String,
         password: String
     ) = repository.handleRegistration(name, email, password)
+
+    fun isPasswordValid(password: String): Boolean {
+        return password.length >= 8
+    }
+
+    fun isNameValid(uName: String): Boolean {
+        return uName.isNotEmpty()
+    }
+
+    fun isEmailValid(email: String): Boolean {
+        return PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()
+    }
+    fun isFormValid(name: String, email: String, password: String): Boolean {
+        return isNameValid(name) && isEmailValid(email) && isPasswordValid(password)
+    }
+
 }
