64b162345df53f682c8876ba97840ad2802aaebb
Gorthdzx <krislaksana123@gmail.com>
2024-10-30 19:15:33 +0700

    delete : user token via di

--- app/src/main/java/app/ditodev/ceritain/data/di/DependencyInjection.kt
+++ app/src/main/java/app/ditodev/ceritain/data/di/DependencyInjection.kt
@@ -5,14 +5,11 @@ import app.ditodev.ceritain.data.pref.UserPreferences
 import app.ditodev.ceritain.data.pref.dataStore
 import app.ditodev.ceritain.data.remote.api.ApiConfig
 import app.ditodev.ceritain.data.repository.StoryRepository
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
 
 object DependencyInjection {
     fun provideRepository(context: Context): StoryRepository {
         val pref = UserPreferences.getInstance(context.dataStore)
-        val user = runBlocking { pref.getSession().first() }
-        val apiService = ApiConfig.getApiService(user.token)
+        val apiService = ApiConfig.getApiService()
         return StoryRepository.getInstance(apiService, pref)
     }
 }
